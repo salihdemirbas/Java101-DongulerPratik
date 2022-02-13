e84409173b0a8c38ffbbf94fb238e1b21c36d20a
Salih Demirbas <salih.demirbas@testinium.com>
2022-02-13 22:58:27 +0300

    BasamakToplama

---
+++ src/com/dongulerPratik/BasamakToplama.java
@@ -0,0 +1,23 @@
+package com.dongulerPratik;
+
+import java.util.Scanner;
+
+public class BasamakToplama {
+    public static void main(String[] args){
+        Scanner girdi = new Scanner(System.in);
+        System.out.println("Lütfen bir tammsayı yazın");
+
+        int sayi = girdi.nextInt();
+        int toplam = 0;
+
+        while (sayi != 0) {
+            toplam += sayi % 10; //sağdaki basamağı toplama ekle
+            sayi=sayi/10; //sağdaki basamağı sil
+        }
+        System.out.println("Yazdığınız sayının basamakları toplamı="+toplam);
+
+
+
+
+    }
+}
